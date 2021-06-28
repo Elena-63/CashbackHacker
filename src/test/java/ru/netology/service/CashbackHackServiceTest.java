76486b678440ba48782be9d3c9179b0d0af6694a
Elena-63 <domanova>
2021-06-28 18:09:50 +0300

    Добавлены автотесты CashbackHackServiceTest.java

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+
+    @Test
+    public void shouldCorrectAmount() {
+        int amount = 2100;
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(actual,expected);
+
+    }
+    @Test
+    public void shouldAmountZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void shouldBuyExtraIfTheAmount1000(){
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual,expected);
+    }
+
+
+}
